4983c79e887e15ea8442162c246e038a0f9c54e3
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2019-10-25 16:56:05 -0300

    Crontab apenas dias da semana e nos horarios do pregao

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const services = require('./services')
 const app = express()
 const port = +(process.env.PORT || 4000)
 
-schedule.scheduleJob('*/5 * * * *', () => {
+schedule.scheduleJob('*/5 10-18 * * 1,2,3,4,5', () => {
   return services.updateData()
 })
 
